6e198806a0090a5e78fa49e8ce865aea9976f48a
sudhakar678 <87324686+sudhakar678@users.noreply.github.com>
2022-01-27 01:08:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("IT WORKED");
+  res.send("IT WORKED by me");
 });
 app.get("/new", function (req, res) {
   res.send("New also worked");
